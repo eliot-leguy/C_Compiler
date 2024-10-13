adb72d68135f51f4073e7d06003135a47876776d
LEGUY Eliot <eliot.leguy@gmail.com>
2024-10-13 21:00:39 +0200

    Just opening file

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
+#include <fstream>
 #include "token/tokenizer.h"
 
-int main()
-{
+int main(int argc, char *argv[]){
+
+	if(argc < 2) return -1;
+
+	try{
+		std::ifstream inputFile(argv[1]);
+		if(!inputFile.is_open()){
+			std::cerr << "Error opening the file " << argv[1] << std::endl;
+		}
+	} catch(...){
+		std::cerr << "An error occured." << std::endl;
+	}
 
 	return 0;
 }
