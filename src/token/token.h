09d39e7ae86be1cef979c5f4eb79bcaf8b656240
LEGUY Eliot <eliot.leguy@gmail.com>
2024-10-13 20:50:48 +0200

    token skeleton

--- src/token/token.h
+++ src/token/token.h
@@ -1,4 +1,23 @@
+#include <string>
+
 class token{
+    protected:
+        enum type{
+            VARIABLE,
+            OPERATOR,
+            KEY_WORD
+        };
 
+    std::string name;
 
+    public:
+        token(token::type, std::string);
+        token(const token&);
+        token(token&&);
+        ~token();
+        token& operator=(const token&);
+        token& operator=(token&&);
+
+        token::type getType();
+        std::string getName();
 };
